caa07ceff454ee2792e6cb9fb0980097c14a10fb
Harrison <hmcguire13@gmail.com>
2018-09-10 07:06:27 -0400

    added simple  mystaticmeshactor

---
+++ MyStaticMeshActor/MyStaticMeshActor.cpp
@@ -0,0 +1,7 @@
+// Harrison McGuire
+// UE4 Version 4.20.2
+// https://github.com/Harrison1/unrealcpp
+// https://severallevels.io
+// https://harrisonmcguire.com
+
+#include "MyStaticMeshActor.h"
---
+++ MyStaticMeshActor/MyStaticMeshActor.h
@@ -0,0 +1,17 @@
+// Harrison McGuire
+// UE4 Version 4.20.2
+// https://github.com/Harrison1/unrealcpp
+// https://severallevels.io
+// https://harrisonmcguire.com
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MyStaticMeshActor.generated.h"
+
+UCLASS()
+class UNREALCPP_API AMyStaticMeshActor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+};
