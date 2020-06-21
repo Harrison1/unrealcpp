c8b9d848b1800e13eb228354a02704772d30448b
Harrison <hmcguire13@gmail.com>
2020-06-21 07:52:11 -0400

    Updated ADestroyActorOnOverlap for version 4.25.1

--- DestroyActorOnOverlap/DestroyActorOnOverlap.h
+++ DestroyActorOnOverlap/DestroyActorOnOverlap.h
@@ -1,11 +1,9 @@
-// Harrison McGuire
-// UE4 Version 4.20.2
-// https://github.com/Harrison1/unrealcpp
-// https://severallevels.io
-// https://harrisonmcguire.com
+// Unreal Version 4.25.1 
+
 
 #pragma once
 
+#include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "DestroyActorOnOverlap.generated.h"
 
@@ -18,7 +16,9 @@ public:
 	// Sets default values for this actor's properties
 	ADestroyActorOnOverlap();
 
-	UFUNCTION()
-	void OnOverlap(AActor* MyOverlappedActor, AActor* OtherActor);
+	UPROPERTY(VisibleAnywhere)
+	class USphereComponent* MySphereComponent;
 
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
